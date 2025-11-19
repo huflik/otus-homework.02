7b61522d60a5edbea3587cd96ef27c780a671f52
huflik <oflik@ya.ru>
2025-11-19 13:26:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "ip_filter.h"
-#include "cin_redirect.h"
 
 constexpr const char *defaultFilename = "./../ip_filter.tsv";
 
@@ -11,15 +10,14 @@ int main(int argc, char *argv[]) {
 	try{
 		
 		IPFilter ipFilter;
-
 		std::ifstream file(filename);
+		
         auto ipPool = ipFilter.getIPAddr(file ? file : std::cin);
 		ipFilter.reverseSort(ipPool);
 		ipFilter.out(ipPool);
 		ipFilter.out(ipFilter.filter(ipPool,1));
 		ipFilter.out(ipFilter.filter(ipPool,46, 70));
 		ipFilter.out(ipFilter.filterAny(ipPool, 46));
-
 
 	} catch (const std::exception &e) {
 		std::cerr << e.what() << std::endl;
